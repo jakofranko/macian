561a2ab18b626e05bd93668703fb28a2c8ae49d6
Jake Franklin <jacob.d.franklin@gmail.com>
2022-03-07 13:38:21 -0700

    Use the local version of macian for docs

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     plugins: [
         require('postcss-import')({}),
         require('mdcss')({
-            theme: require('mdcss-theme-macian'),
+            theme: require('mdcss-theme-macian')({ macianLocation: './macian.min.css' }),
             destination: 'docs',
             assets: ['./macian.min.css']
         }),
